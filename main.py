dcc387a93fe12b91a5a4b7d4d73fee457a06901b
Venukarthik17 <144209708+Venukarthik17@users.noreply.github.com>
2023-09-14 16:41:07 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-def fact(n):
-  if n == 0 or n == 1:
-    return 1
-  else:
-    return n * fact(n - 1)
-
-
-number = int(input("enter a value:"))
-res = fact(number)
-print("the factorial of {} is {}.".format(number, res))
+# Python program to create Bankaccount class
+class Bank_Account:
+    def __init__(self):
+        self.balance=0
+        print("Bank Account is 2324543098575")
+ 
+    def deposit(self):
+        amount=float(input("Enter amount to be Deposited: "))
+        self.balance += amount
+        print("\n Amount Deposited:",amount)
+ 
+    def withdraw(self):
+        amount = float(input("Enter amount to be Withdrawn: "))
+        if self.balance>=amount:
+            self.balance-=amount
+            print("\n You Withdrew:", amount)
+        else:
+            print("\n Insufficient balance  ")
+ 
+    def display(self):
+        print("\n Net Available Balance=",self.balance)
+ 
+# Driver code
+  
+# creating an object of class
+s = Bank_Account()
+  
+# Calling functions with that class object
+s.deposit()
+s.withdraw()
+s.display()
